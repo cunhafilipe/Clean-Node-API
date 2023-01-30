9af0fb5c92ca441008ee059ad2a4f702d6075956
Luis Filipe <luiiz_fillipe@hotmail.com>
2023-01-30 11:52:05 -0300

    chore: add jest to lint staged

--- jest.config.ts
+++ jest.config.ts
@@ -20,7 +20,7 @@ export default {
   collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: ['<rootDir/src/**/*.ts>'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: "coverage",
@@ -144,7 +144,7 @@ export default {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  testEnvironment: "jest-environment-node",
+  testEnvironment: "node",
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
